a0c3259074238d5bcc99ba7897331646dfe19c28
Godric2003D <66315809+Godric2003D@users.noreply.github.com>
2021-10-31 15:07:03 +0530

    first DR

---
+++ Debayan/home.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "35px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
