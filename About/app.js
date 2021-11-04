4c252a064c65f4c639c0683c3e6d284e27012a0e
shovikgod <shovikmetal@gmail.com>
2021-11-04 11:35:20 +0530

    Merged

--- About/app.js
+++
@@ -1,9 +0,0 @@
-let line1=document.querySelector('.line-1');
-let line2=document.querySelector('.line-2');
-
-
-window.onscroll=()=>{
-    let pos=window.scrollY-800;
-    line1.style.left=`${pos}px`;
-    line2.style.right=`${pos}px`;
-}
